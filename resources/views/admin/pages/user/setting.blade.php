a8a7709a30a8178bb13f9fb4b8a43f8ea67e78b7
Arwa Eka Aulia <ekaaulia1603@gmail.com>
2019-02-07 10:52:10 +0700

    episode 9

--- resources/views/admin/pages/user/setting.blade.php
+++ resources/views/admin/pages/user/setting.blade.php
@@ -3,34 +3,50 @@
 @section('content')
 <h1>User</h1>
 <hr>
+
+@if(session('result') == 'success')
+<div class="alert alert-success alert-dismissiable fade show">
+	<strong>Update !</strong> Berhasil diupdate.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;  
+	</button>
+</div>
+@elseif(session('result') == 'fail')
+<div class="alert alert-danger alert-dismissiable fade show">
+	<strong>Failed !</strong> Gagal diupdate.
+	<button type="button" class="close" data-dismiss="alert">
+		&times;  
+	</button>
+</div>
+@endif
+
 <div class="row">
 	<div class="col-md-6">
-		<form method="post" action="{{ route('admin.user.setting') }}"> 
+		<form method="post" action="{{ route('admin.user.setting')}}">
 			<div class="card mb-3">
 				<div class="card-header"><h5>Setting</h5></div>
 				<div class="card-body">
 					{{ csrf_field() }}
-
 					<div class="form-group form-label-group">
 						<input type="text" name="name"
 						class="form-control {{$errors->has('name')?'is-invalid':''}}"
-						value="{{ old('name',$dt->name) }}"
+						value="{{ old('name',$dt->name)}}"
 						id="iName" placeholder="Name" required>
 						<label for="iName">Name</label>
 						@if($errors->has('name'))
 						<div class="invalid-feedback">{{$errors->first('name')}}</div>
 						@endif
 					</div>
-
 					<div class="form-group form-label-group">
 						<input type="text" name="email"
 						class="form-control {{$errors->has('email')?'is-invalid':''}}"
-						value="{{ old('email',$dt->email) }}"
+						value="{{ old('email',$dt->email)}}"
 						id="iEmail" placeholder="Email" required>
 						<label for="iEmail">Email</label>
 						@if($errors->has('email'))
 						<div class="invalid-feedback">{{$errors->first('email')}}</div>
 						@endif
+						
 					</div>
 
 					<div class="form-group form-label-group">
@@ -44,8 +60,8 @@
 						<div class="form-text text-muted">
 							<small>Kosongkan Password apabila tidak diubah.</small>
 						</div>
-					</div>
 
+					</div>
 					<div class="form-group form-label-group">
 						<input type="password" name="repassword"
 						class="form-control {{$errors->has('repassword')?'is-invalid':''}}"
@@ -54,16 +70,21 @@
 						@if($errors->has('repassword'))
 						<div class="invalid-feedback">{{$errors->first('repassword')}}</div>
 						@endif
+						
+
 					</div>
-
-
+					
 				</div>
 				<div class="card-footer">
 					<button type="submit" class="btn btn-primary shadow-sm">Update</button>
+
 				</div>
 			</div>
-
 		</form>
 	</div>
 </div>
+
+
+
+
 @endsection
